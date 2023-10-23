17cb38219a366d23d7d80ac682283976b8451a41
ayghub <91691648+ayghub@users.noreply.github.com>
2023-10-24 00:06:40 +0100

    Update open-dir.lua

--- open-dir.lua
+++ open-dir.lua
@@ -1,9 +1,9 @@
-local utils = require "mp.utils"
+local utils = require 'mp.utils'
 
 function open_dir()
-	local path = mp.get_property("path")
-	local current_path, _ = utils.split_path(path)
-	os.execute(string.format("explorer %s", current_path))
+    local path = mp.get_property("path")
+    local current_path, _ = utils.split_path(path)
+    os.execute(string.format('start "" "%s"', current_path))
 end
 
 mp.add_key_binding("ctrl+d", "open_current_dir", open_dir)
